e73dc786f5f4f334fb06d18a2150049f52744caf
21700340JuwonBaek <21700340@handong.edu>
2020-05-18 01:01:32 +0900

    동빈나 3강까지 구현

---
+++ src/rythmGame/DynamicBeat.java
@@ -0,0 +1,45 @@
+package rythmGame;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+public class DynamicBeat extends JFrame{
+
+	
+	private Image screenImage;//
+	private Graphics screenGraphic;
+	//전체 화면에 대해서 더블 버퍼링을 사용하기 위해 이미지를 담는 instance...
+	
+	
+	private Image introBackground;
+	//받아온 이미지를 담는 객체
+	
+	
+		public DynamicBeat() {
+			setTitle("Dynamic Beat");
+			setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+			setResizable(false);
+			setLocationRelativeTo(null);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			
+			
+			introBackground = new ImageIcon(Main.class.getResource("../images/IntroBackground.jpg")).getImage();
+			Music introMusic = new Music("introMusic.mp3",true);
+			introMusic.start();
+		}
+		
+		public void paint(Graphics g) {
+			screenImage = createImage(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+			screenGraphic = screenImage.getGraphics();
+			screenDraw(screenGraphic);
+			g.drawImage(screenImage,0,0,null);
+		}
+		
+		public void screenDraw(Graphics g) {
+			g.drawImage(introBackground, 0, 0, null);
+			this.repaint();
+		}
+}
